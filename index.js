c5bbb175cd8fba914602658d11501423d9b321ff
pramesh-gawas <prameshgawas11@gmail.com>
2025-12-22 23:59:17 +0530

    added route for admin

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const nodemailer = require("nodemailer");
 const express = require("express");
-const User = require("./models/user");
-const { generateToken, jwtAuthMiddleWare } = require("./jwt");
 const db = require("./db");
 require("dotenv").config();
 const app = express();
@@ -17,6 +15,52 @@ app.use(
 );
 
 app.use(express.json());
+
+app.post("/sendMail", async (req, res) => {
+  try {
+    const { email, obj } = req.body;
+    console.log(obj, email);
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.RESETEMAIL, // env
+        pass: process.env.RESETPASSWORD, //env
+      },
+    });
+
+    mailOptions = {
+      from: {
+        name: email,
+        address: process.env.RESETEMAIL,
+      },
+      to: process.env.RESETEMAIL,
+      replyTo: email,
+      subject: "Mail from the Portfolio",
+      html: `
+    <div style="font-family: 'Inter', sans-serif; line-height: 1.6; color: #333; max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 10px;">
+     <p><strong>From:</strong> ${email}</p>
+      <p>${obj}</p>
+    </div>
+  `,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        return res.send({
+          error: "Error Sending Email",
+        });
+      } else {
+        return res.send({
+          message: "Email has been sent to Pramesh Gawas",
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 const adminRoutes = require("./routes/adminRoutes");
 app.use("/admin", adminRoutes);
